782d52d7d86a692f542ef82fa07d693714bbafd0
Silureth <szilard.herczeg@gmail.com>
2022-11-23 17:02:41 +0100

    4 files modified, 2 added

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
-import { platformNativeScript, runNativeScriptAngularApp } from '@nativescript/angular';
+import {
+  platformNativeScript,
+  runNativeScriptAngularApp,
+} from '@nativescript/angular';
 
 import { AppModule } from './app/app.module';
+
+// added this here so we can do some wiring -- without this, you won're receive a push token on iOS
+require('nativescript-plugin-firebase');
 
 runNativeScriptAngularApp({
   appModuleBootstrap: () => platformNativeScript().bootstrapModule(AppModule),
 });
-
